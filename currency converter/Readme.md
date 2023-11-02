62ff797647602baec69779f2a7c9800a9da5d721
TahreemFeroz19 <59493300+TahreemFeroz19@users.noreply.github.com>
2023-11-02 13:27:41 +0500

    currency converter

---
+++ currency converter/Readme.md
@@ -0,0 +1,25 @@
+
+# CURRENCY CONVERTER
+
+The Currency Converter is a simple and user-friendly tool that allows you to quickly convert between two commonly used currencies: USD (United States Dollar) and PKR (Pakistani Rupee). Whether you're traveling, making international transactions, or just curious about the current exchange rate, this tool provides a convenient way to perform currency conversions.
+
+
+## Features
+
+- Convert from USD to PKR: Input an amount in US Dollars, and the converter will provide the equivalent amount in Pakistani Rupees.
+- Convert from PKR to USD: If you have an amount in Pakistani Rupees and need to know its value in US Dollars, simply enter the PKR amount, and the converter will give you the USD equivalent.
+
+Enjoy hassle-free currency conversion with the Currency Converter project.
+
+
+
+## Deployment
+
+To deploy this project run
+
+```bash
+  npx currency-converter-by-tahreem
+```
+
+## Screenshot
+![Alt text](image.png)
---
+++ currency converter/index.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+let sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 3000);
+    });
+};
+async function welcome() {
+    const karaoke = chalkAnimation.karaoke('CURRENCY CONVERTER, \nconvert your money into any currency'); // Animation starts
+    await sleep();
+    karaoke.stop();
+}
+await welcome();
+let convertFrom = await inquirer
+    .prompt({
+    type: "list",
+    name: "conversionfrom",
+    message: "Convert From: ",
+    choices: ["USD", "PKR"]
+});
+let convertInto = await inquirer
+    .prompt({
+    type: "list",
+    name: "conversioninto",
+    message: "Convert into: ",
+    choices: ["USD", "PKR"]
+});
+let amount = await inquirer
+    .prompt({
+    type: "number",
+    name: "addamount",
+    message: "Enter amount: "
+});
+if (convertFrom.conversionfrom == "USD" && convertInto.conversioninto == "PKR") {
+    console.log(`Your amount is ${amount.addamount * 278.97}PKR`);
+}
+else if (convertFrom.conversionfrom == "PKR" && convertInto.conversioninto == "USD") {
+    console.log(`Your amount is ${amount.addamount * 0.036}USD`);
+}
+else {
+    console.log(`${amount.addamount}`);
+}
